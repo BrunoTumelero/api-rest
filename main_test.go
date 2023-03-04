922cbaba8fcd90a9c44874fd52597325fb01f83e
BrunoTumelero <Bruno.Tumelero@outlook.com>
2023-03-04 17:00:19 -0300

    test endpoints

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"api-rest/controlers"
+	"api-rest/database"
+	"api-rest/models"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -11,9 +13,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func SetupRoutes() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	rotas := gin.Default()
 	return rotas
+}
+
+func CreatStudentMock() {
+	student := models.Aluno{Nome: "Aluno teste", CPF: "12345678910",
+		RG: "123654789"}
+	database.DB.Create(&student)
+	ID = int(student.ID)
+}
+
+func DeleteStudentMock() {
+	var student models.Aluno
+	database.DB.Delete(&student, ID)
 }
 
 func TestVerificationStatus(t *testing.T) {
@@ -26,4 +43,28 @@ func TestVerificationStatus(t *testing.T) {
 	mockResposta := `{"API diz": "Seja bem vindo " + nome}`
 	respostaBody, _ := ioutil.ReadAll(resposta.Body)
 	assert.Equal(t, mockResposta, string(respostaBody))
+}
+
+func TestListstudents(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CreatStudentMock()
+	defer DeleteStudentMock()
+	r := SetupRoutes()
+	r.GET("/alunos", controlers.TodosAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
+
+func TestCPF(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CreatStudentMock()
+	defer DeleteStudentMock()
+	r := SetupRoutes()
+	r.GET("/alunos/cpf/:cpf", controlers.FindCPF)
+	req, _ := http.NewRequest("GET", "alunos/cpf/numero do cpf", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
 }
