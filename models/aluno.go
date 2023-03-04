f071280e6bd39ec42476e7f8a9809283c2597381
BrunoTumelero <Bruno.Tumelero@outlook.com>
2023-03-03 21:39:51 -0300

    start tests

--- models/aluno.go
+++ models/aluno.go
@@ -1,10 +1,20 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gopkg.in/validator.v2"
+	"gorm.io/gorm"
+)
 
 type Aluno struct {
 	gorm.Model
-	Nome string `json:"nome"`
-	CPF  string `json:"nome"`
-	RG   string `json:"nome"`
+	Nome string `json:"nome" validate:"nonzero"`
+	CPF  string `json:"nome" validate:"len=10"`
+	RG   string `json:"nome" validate:"len=9"`
+}
+
+func Validation(student *Aluno) error {
+	if err := validator.Validate(student); err != nil {
+		return err
+	}
+	return nil
 }
